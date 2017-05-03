96e85ca934e4605f336fa34515a0a34baeec1398
zenmaibane <koteicream.1024@gmail.com>
2017-05-03 17:31:24 +0900

    つらい

---
+++ main.js
@@ -0,0 +1,60 @@
+function requestJson(text) {
+    let requestUrl = "https://socialityfilter.takanakahiko.me/?text=" + text;
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", requestUrl, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            let json = JSON.parse(xhr.responseText);
+            json = JSON.parse(json);
+            console.log(json.response);
+            let tweetTextArea = document.querySelector(".compose-content .js-compose-text");
+            tweetTextArea.value = json.response;
+            tweetTextArea.focus()
+            let length = tweetTextArea.value.length
+            tweetTextArea.selectionStart = length;
+            tweetTextArea.selectionEnd = length;
+            $('textarea').trigger(
+                jQuery.Event( 'keydown', { keyCode: 13, which: 13 } )
+                );
+        }
+    };
+    xhr.send();
+}
+function translateToNyaan(){
+    if(!$('#filter-button').hasClass('is-disabled')){
+        let tweetTextArea = document.querySelector(".compose-content .js-compose-text");
+        requestJson(tweetTextArea.value);
+    }
+}
+
+$(function () {
+    setTimeout(function () {
+        console.log("test");
+        let tweetButton = $(".js-send-button-container");
+        let filterButton = $("<button></button>", {
+            id: "filter-button",
+            class: "js-send-button js-spinner-button js-show-tip btn btn-positive btn-extra-height is-disabled",
+            text:"< Nyaan",
+            "data-original-title":"Nyaan (alt+n)"
+        });
+        tweetButton.append(filterButton);
+        $('.compose-content .js-compose-text').keyup(function() {
+            var val = $(this).val();
+            if(val.length === 0){
+                $("#filter-button").addClass("is-disabled")
+            }else{
+                $("#filter-button").removeClass("is-disabled");
+            }
+        });
+        $("#filter-button").on("click", function () {
+                 translateToNyaan();
+        });
+    }, 1000);
+
+     $(document).keydown(function(e){
+         console.log(e.keyCode)
+         if(e.keyCode===78 && e.altKey){//alt+nキー
+                 translateToNyaan();
+            }
+        });
+     });

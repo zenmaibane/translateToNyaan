2f3c641ed5b712ab7c428c7edec853a04aee4e02
sydosy <sydosy@gmail.com>
2017-10-21 04:57:11 +0900

    リファクタリング

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-let nyaanInitText = "< Nyaan";
-let nyaanInitColor = "#80c0a0";
+const nyaanInitText = "< Nyaan";
+const nyaanInitColor = "#80c0a0";
 
 run();
+
 function run() {
-    let tweetButton = document.querySelector("button.js-send-button");//jqueryオブジェクトにするとobserve出来ない
-    if (tweetButton == null) {
+    const tweetButton = document.querySelector("button.js-send-button");//jqueryオブジェクトにするとobserve出来ない
+    if (tweetButton === null) {
         setTimeout(run, 1500);
         return;
     }
 
-    let tweetButtonContainer = $(".js-send-button-container");
-    let filterButton = $("<button></button>", {
+    const tweetButtonContainer = $(".js-send-button-container");
+    const filterButton = $("<button></button>", {
         "id": "filter-button",
         "class": "js-send-button js-spinner-button js-show-tip btn btn-positive btn-extra-height is-disabled",
         "text": nyaanInitText,
@@ -25,30 +26,30 @@ function run() {
     filterButton.on("click", function () {
         translateToNyaan();
     });
-    let tweetButtonObserver = new MutationObserver(function () {
+    const tweetButtonObserver = new MutationObserver(function () {
         if (tweetButton.classList.contains("is-disabled")) {
-            filterButton.addClass("is-disabled")
+            filterButton.addClass("is-disabled");
             filterButton.text(nyaanInitText)
         } else {
             filterButton.removeClass("is-disabled")
         }
-        filterButton.css({"background-color":nyaanInitColor})
+        filterButton.css({"background-color": nyaanInitColor})
     });
     tweetButtonObserver.observe(tweetButton, {
         'attributes': true,
         "attributeFilter": ["class"]
     });
 
-    let tweetTextArea = document.querySelector("textarea.js-compose-text");//jqueryオブジェクトにするとobserve出来ない
-    let tweetObserver = new MutationObserver(function () {
+    const tweetTextArea = document.querySelector("textarea.js-compose-text");//jqueryオブジェクトにするとobserve出来ない
+    const tweetObserver = new MutationObserver(function () {
         if (tweetTextArea.disabled) {
             filterButton.text("")
         }
         else {
-            filterButton.css({"background-color":nyaanInitColor})
-            filterButton.text(nyaanInitText)
+            filterButton.css({"background-color": nyaanInitColor});
+            filterButton.text(nyaanInitText);
         }
-    })
+    });
     tweetObserver.observe(tweetTextArea, {
         "attributes": true,
         "attributeFilter": ["disabled"]
@@ -57,31 +58,34 @@ function run() {
 
 function translateToNyaan() {
     if (!$('#filter-button').hasClass('is-disabled')) {
-        let tweetTextArea = $('textarea.js-compose-text')[0];
-        requestJson(tweetTextArea.value);
+        const tweetTextArea = $('textarea.js-compose-text')[0];
+        requestJson(tweetTextArea.value, function () {
+            const json = JSON.parse(this.responseText);
+            tweetTextArea.value = json.response;
+            tweetTextArea.dispatchEvent(new Event('change'));
+        });
     }
 }
 
-function requestJson(text) {
-    let requestUrl = "https://socialityfilter.takanakahiko.me/?text=" + text;
-    let xhr = new XMLHttpRequest();
-    let filterButton = $("#filter-button");
-    xhr.open("GET", requestUrl, true);
-    filterButton.css({"background-color":nyaanInitColor})
-    filterButton.text("Nyaaning...")
+function requestJson(text, callback) {
+    const requestUrl = "https://socialityfilter.takanakahiko.me/?text=" + text;
+    const xhr = new XMLHttpRequest();
+    xhr.callback = callback;
+    const filterButton = $("#filter-button");
+    filterButton.css({"background-color": nyaanInitColor});
+    filterButton.text("Nyaaning...");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
-            if (xhr.status == 200 || xhr.status == 304) {
-                let json = JSON.parse(xhr.responseText);
-                let tweetTextArea = $('textarea.js-compose-text')[0];
-                tweetTextArea.value = json.response;
-                tweetTextArea.dispatchEvent(new Event('change'));
-                filterButton.text("Success!")
+            if (xhr.status === 200 || xhr.status === 304) {
+                xhr.callback.apply(this);
+                filterButton.text("Success!");
             } else {
-                filterButton.css({"background-color":"#F14C4A"})
-                filterButton.text("Failed")
+                filterButton.css({"background-color": "#F14C4A"});
+                filterButton.text("Failed");
             }
         }
     };
+
+    xhr.open("GET", requestUrl, true);
     xhr.send();
 }
